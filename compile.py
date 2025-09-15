4f779bace693e5b4c4c57aacd3ce65133e222605
2bt <s8572327@gmail.com>
2025-09-15 18:40:23 +0200

    wip struct

--- compile.py
+++ compile.py
@@ -6,34 +6,68 @@ import argparse
 from collections import namedtuple
 from types import SimpleNamespace
 
-Token    = namedtuple("Token",    "k v file line")
+"""
+idea: transform ast
+
+    var a: int[4]
+    a[2] = 4    -->     *(a + 2) = 4
+
+    var q: int*
+    q = &a[3]   -->     q = a + 3
+    *q = 4
+    q += 1
+
+    mov q, #a + 3
+    mov [q], #4
+    add q, 1
+
+    &a[3]
+
+
+    &:
+    + works on lvalues: VarRef, Index, Deref, Field
+    + inc ptr
+
+    *:
+    + works on pointers
+    + ptr > 0 --> dec ptr
+
+"""
+
+
+
+
+Token    = namedtuple("Token",    "k v loc")
+
+Type     = namedtuple("Type",     "base ptr array")
+TyStruct = namedtuple("TyStruct", "fields size")
+TyField  = namedtuple("TyField",  "type offset")
 
 Imm      = namedtuple("Imm",      "value")
 VarRef   = namedtuple("VarRef",   "name")
-Index    = namedtuple("Index",    "name idx")
-Neg      = namedtuple("Neg",      "expr")
+Index    = namedtuple("Index",    "base idx")
+Field    = namedtuple("Field",    "base name")
+AddrOf   = namedtuple("AddrOf",   "lv")
+Deref    = namedtuple("Deref",    "expr")
 BinOp    = namedtuple("BinOp",    "op a b")
 Call     = namedtuple("Call",     "name args")
 
-Var      = namedtuple("Var",      "name addr")
-VarArr   = namedtuple("VarArr",   "name size addr")
-Func     = namedtuple("Func",     "name args body")
+Var      = namedtuple("Var",      "name type addr")
+Func     = namedtuple("Func",     "name args type body")
 
 Assign   = namedtuple("Assign",   "lhs op rhs")
 While    = namedtuple("While",    "cond body")
 Break    = namedtuple("Break",    "")
 Continue = namedtuple("Continue", "")
-If       = namedtuple("If",       "cond then else_")
+If       = namedtuple("If",       "cond then els")
 Ret      = namedtuple("Ret",      "expr")
 Asm      = namedtuple("Asm",      "asm")
 
 
 KEYWORDS = {
-    "include",
-    "var", "const",
+    "include", "var", "const", "func", "struct",
     "if", "then", "elif", "else", "while", "do", "end", "break", "continue",
-    "or", "and",
-    "func", "return", "asm",
+    "return", "asm", "or", "and",
 }
 
 
@@ -42,7 +76,7 @@ token_regex = re.compile(r"""
     [ \t]*(?P<comment> \#.*                        )|
     [ \t]*(?P<num>     \$[0-9A-Fa-f]+|[0-9]+       )|
     [ \t]*(?P<id>      [A-Za-z_][A-Za-z0-9_]*      )|
-    [ \t]*(?P<sym>     ==|!=|<=|>=|\|=|&=|\+=|-=|\*=|/=|%=|[+\-*/%()<>\[\]=,:&|@])|
+    [ \t]*(?P<sym>     ==|!=|<=|>=|\|=|&=|\+=|-=|\*=|/=|%=|[+\-*/%{}()<>\[\]=,.:&|@])|
     [ \t]*(?P<string>  "(?:[^"]|\\.)*"             )|
     [ \t]*(?P<other>   [^ \t]+                     )
 """, re.VERBOSE)
@@ -59,8 +93,8 @@ def tokenize(path):
         if k == "nl": line += 1; continue
         if k == "string": line += v.count("\n")
         if k == "id" and v in KEYWORDS: k = v
-        out.append(Token(k, v, path, line))
-    out.append(Token("eof", "", path, line))
+        out.append(Token(k, v, (path, line)))
+    out.append(Token("eof", "", (path, line)))
     return out
 
 
@@ -114,15 +148,15 @@ class Parser:
         self.consts   = {}
         self.decls    = {}
         self.funcs    = {}
+        self.structs  = {}
         # for includes
         self.included = set()
         self.stack    = []
 
-
-    def error(self, msg, t=None):
-        t = t or self.peek()
-        line = t.file.read_text().split("\n")[t.line - 1]
-        sys.exit(f"parse error: {msg}\n{t.file}:{t.line}:{line}")
+    def error(self, msg, loc=None):
+        file, nr = loc or self.peek().loc
+        line = file.read_text().split("\n")[nr - 1]
+        sys.exit(f"{msg}\n{file.name}:{nr}:{line}")
 
     def peek(self, k=None, v=None):
         tok = self.head.toks[self.head.i]
@@ -141,28 +175,44 @@ class Parser:
 
     def const_expr(self):
         n = self.expr()
-        if not isinstance(n, Imm):
-            self.error("const expression expected")
+        if not isinstance(n, Imm): self.error("const expression expected")
         return n.value
 
-    def var_address(self):
+    def type(self):
+        if not self.peek("sym", ":"): return Type("int", 0, None)
+        self.eat()
+        base = self.eat("id").v
+        ptr = 0
+        while self.peek("sym", "*"): self.eat(); ptr += 1
+        array = None
+        if self.peek("sym", "["):
+            self.eat()
+            array = self.const_expr()
+            self.eat("sym", "]")
+        return Type(base, ptr, array)
+
+    def type_size(self, t):
+        if t.ptr > 0: return 1
+        n = t.array or 1
+        e = 1 if t.base == "int" else self.structs[t.base].size
+        return n * e
+
+    def add_decl(self, prefix=""):
+        name = prefix + self.eat("id").v
+        if name in self.decls: self.error(f"variable '{name}' already exists")
+        type = self.type()
+        addr = None
         if self.peek("sym", "@"):
             self.eat()
-            return self.const_expr()
-        return None
-
-
-    def add_decl(self, v):
-        if v.name in self.decls:
-            self.error(f"variable '{v.name}' already exists")
-        self.decls[v.name] = v
-
+            addr = self.const_expr()
+        self.decls[name] = Var(name, type, addr)
+        return name
 
     def program(self, path):
         self.included.add(path)
         self.head = SimpleNamespace(toks = tokenize(path), i = 0)
 
-        while any(self.peek(k) for k in ["include", "const", "var", "func"]):
+        while any(self.peek(k) for k in ["include", "const", "var", "func", "struct"]):
             tok = self.eat()
             if tok.k == "include":
                 incl = path.parent / ast.literal_eval(self.eat("string").v)
@@ -177,39 +227,40 @@ class Parser:
                 self.consts[name] = self.const_expr()
 
             elif tok.k == "var":
-                name = self.eat("id").v
-                if self.peek("sym", "["):
-                    self.eat()
-                    n = self.const_expr()
-                    self.eat("sym", "]")
-                    self.add_decl(VarArr(name, n, self.var_address()))
-                else:
-                    self.add_decl(Var(name, self.var_address()))
+                self.add_decl()
 
-            else:
-                # function definition
+            elif tok.k == "struct":
                 name = self.eat("id").v
-                if name in self.funcs: self.error("function already exists")
+                if name in self.structs: self.error(f"struct '{name}' already exists")
+                fields = {}
+                offset = 0
+                while not self.peek("end"):
+                    n = self.eat("id").v
+                    t = self.type()
+                    fields[n] = TyField(t, offset)
+                    offset += self.type_size(t)
+                self.eat("end")
+                self.structs[name] = TyStruct(fields, offset)
+
+            elif tok.k == "func":
+                name = self.eat("id").v
+                if name in self.funcs: self.error(f"function '{name}' already exists")
                 self.local_var_prefix = f"_{name}_"
                 self.eat("sym", "(")
                 args = []
-                def add_arg():
-                    arg = self.local_var_prefix + self.eat("id").v
-                    self.add_decl(Var(arg, self.var_address()))
-                    args.append(arg)
                 if not self.peek("sym", ")"):
-                    add_arg()
+                    args.append(self.add_decl(self.local_var_prefix))
                     while self.peek("sym", ","):
                         self.eat()
-                        add_arg()
+                        args.append(self.add_decl(self.local_var_prefix))
                 self.eat("sym", ")")
+                type = self.type()
                 body = self.block()
                 self.eat("end")
-                self.funcs[name] = Func(name, args, body)
+                self.funcs[name] = Func(name, args, type, body)
 
+            else: assert False
         self.eat("eof")
-        return self.decls, self.funcs
-
 
     def block(self):
         stmts = []
@@ -240,6 +291,12 @@ class Parser:
             body = self.block()
             self.eat("end")
             return While(cond, body)
+        if self.peek("break"):
+            self.eat()
+            return Break()
+        if self.peek("continue"):
+            self.eat()
+            return Break()
         if self.peek("if"):
             self.eat()
             return self.after_if()
@@ -251,19 +308,11 @@ class Parser:
             self.eat()
             return Asm(self.eat("string").v)
 
-        if self.peek("break"):
-            self.eat()
-            return Break()
-        if self.peek("continue"):
-            self.eat()
-            return Break()
-
         lhs = self.primary()
-        if isinstance(lhs, Call):
-            return lhs
+        if isinstance(lhs, Call): return lhs
 
         op = self.peek("sym")
-        if (isinstance(lhs, VarRef) or isinstance(lhs, Index)) and op.v in ASSIGN_OPS:
+        if isinstance(lhs, (VarRef, Deref, Index, Field)) and op.v in ASSIGN_OPS:
             op = self.eat().v
             rhs = self.expr()
             return Assign(lhs, op, rhs)
@@ -286,6 +335,9 @@ class Parser:
         return left
 
     def unary(self):
+        if self.peek("sym", "&"):
+            self.eat()
+            return AddrOf(self.primary())
         if self.peek("sym", "("):
             self.eat()
             e = self.expr()
@@ -294,9 +346,8 @@ class Parser:
         if self.peek("sym", "-"):
             self.eat()
             u = self.unary()
-            if isinstance(u, Imm):
-                return Imm(-u.value)
-            return Neg(u)
+            if isinstance(u, Imm): return Imm(-u.value)
+            return BinOp("*", u, Imm(-1))
         return self.primary()
 
     def primary(self):
@@ -318,40 +369,39 @@ class Parser:
             args = []
             if not self.peek("sym", ")"):
                 args.append(self.expr())
-                while self.peek("sym", ","):
-                    self.eat()
-                    args.append(self.expr())
+                while self.peek("sym", ","): self.eat(); args.append(self.expr())
             self.eat("sym", ")")
-
             f = self.funcs.get(name)
-            if not f:
-                self.error("unknown function")
-            if len(args) != len(f.args):
-                self.error("wrong number of arguments")
-
-
+            if not f: self.error(f"unknown function '{name}'")
+            if len(args) != len(f.args): self.error("wrong number of function arguments")
             return Call(name, args)
 
+        # resolve name
         if name not in self.decls:
             old = name
             name = self.local_var_prefix + name
-            if name not in self.decls:
-                self.error(f"unknown variable '{old}'")
+            if name not in self.decls: self.error(f"unknown variable '{old}'")
 
-        # indexing
-        if self.peek("sym", "["):
-            self.eat()
-            idx = self.expr()
-            self.eat("sym", "]")
-            return Index(name, idx)
-
-        return VarRef(name)
+        node = VarRef(name)
+        while True:
+            if self.peek("sym", "["):
+                self.eat()
+                node = Index(node, self.expr())
+                self.eat("sym", "]")
+            elif self.peek("sym", "@"):
+                self.eat()
+                node = Deref(node)
+            elif self.peek("sym", "."):
+                self.eat()
+                node = Field(node, self.eat("id").v)
+            else: return node
 
 
 
 
 
 class Codegen:
+
     def emit(self, s):
         self.lines.append(s)
 
@@ -377,15 +427,21 @@ class Codegen:
             i -= 1
         self.emit(f"{l}:")
 
-    def addr_of_lvalue(self, node):
-        if isinstance(node, VarRef): return node.name
-        if isinstance(node, Index):
-            v = self.value(node.idx)
-            t = self.newtmp(v)
-            if t != v: self.emit(f"    mov {t}, {v}")
-            self.emit(f"    add {t}, #{node.name}")
-            return f"[{t}]"
-        sys.exit("unsupported lvalue")
+
+    def get_type(self, expr):
+        i = id(expr)
+        if t := self.expr_types.get(i): return t
+        if isinstance(expr, Imm): t = ("int", 0, None)
+        elif isinstance(expr, VarRef): t = self.decls[expr.name].type
+        elif isinstance(expr, Index):
+            s = self.get_type(expr.base)
+            assert s.array
+            assert s.ptr == 0
+            return s.base
+
+
+        self.expr_types[i] = t
+        return t
 
 
     def branch(self, node, T, F):
@@ -437,21 +493,52 @@ class Codegen:
 
 
     def value(self, node):
+        if isinstance(node, Imm): return f"#{node.value}"
         if isinstance(node, VarRef): return node.name
         if isinstance(node, Index):
-            v = self.value(node.idx)
-            t = self.newtmp(v)
-            if t != v: self.emit(f"    mov {t}, {v}")
-            self.emit(f"    add {t}, #{node.name}")
-            return f"[{t}]"
-        if isinstance(node, Neg):
-            v = self.value(node.expr)
-            t = self.newtmp(v)
-            if t != v: self.emit(f"    mov {t}, {v}")
-            self.emit(f"    mul {t}, #-1")
-            return t
+            # t = self.get_type(node.base)
+            base = node.base
+            if isinstance(base, VarRef):
+                type = self.decls[base.name].type
+                assert type.array, "must be array"
+                elem_size = self.ast.type_size(Type(type.base, type.ptr, None))
+                if isinstance(node.idx, Imm):
+                    return f"{base.name}+{node.idx.value * elem_size}"
+                v = self.value(node.idx)
+                t = self.newtmp(v)
+                if t != v: self.emit(f"    mov {t}, {v}")
+                if elem_size > 0: self.emit(f"    mul {t}, #{elem_size}")
+                self.emit(f"    add {t}, #{lv.name}")
+                return f"[{t}]"
 
-        if isinstance(node, Imm): return f"#{node.value}"
+            else: assert False, f"TODO: support Deref, Field (type is {type(node).__name__})"
+
+        # if isinstance(node, Field):
+        #     t = self.get_type(node.base)
+        #     assert t.base in self.structs
+        #     assert t.ptr == 0
+        #     assert t.array == None
+        #     v = self.value(node.base)
+        #     field = self.structs[t.base].fields[node.name]
+        #     print("######", field)
+        #     exit(1)
+
+        if isinstance(node, AddrOf):
+            if isinstance(node.lv, Index):
+                v = self.value(node.lv)
+                if v.startswith("["): return v[1:-1]
+                return f"#{v}"
+
+            else: assert False
+
+        if isinstance(node, Deref):
+            v = self.value(node.expr)
+            if not v.startswith("["): return f"[{v}]"
+            t = self.newtmp(v)
+            if t != v: self.emit(f"    mov {t}, {v}")
+            return f"[{t}]"
+
+
         if isinstance(node, BinOp):
             if op := ARITH_OPS.get(node.op):
                 a = self.value(node.a)
@@ -465,7 +552,6 @@ class Codegen:
                     self.emit(f"    mov {t}, {a}")
                     self.emit(f"    {op} {t}, {b}")
                 return t
-
             if node.op in CMP_TO_JMP or node.op in {"and", "or"}:
                 t = self.newtmp()
                 T = self.label("true")
@@ -479,13 +565,11 @@ class Codegen:
                 self.emit(f"    mov {t}, #1")
                 self.emit_label(D)
                 return t
+            assert False, "unsupported binop {node.op}"
 
-            sys.exit(f"unsupported binop {node.op}")
+        if isinstance(node, Call): return self.call(node)
 
-        if isinstance(node, Call):
-            return self.call(node)
-
-        sys.exit("unsupported expression node")
+        assert False, f"unsupported expression {node}"
 
 
     def call(self, node):
@@ -497,14 +581,16 @@ class Codegen:
         self.emit(f"    jsr {f.name}")
         return "_R"
 
+
     def stmt(self, node):
         self.tmp_i = 0 # reuse temporary variables
 
         if isinstance(node, Assign):
-            dst = self.addr_of_lvalue(node.lhs)
+            assert isinstance(node.lhs, (VarRef, Index, Deref, Field))
+            dst = self.value(node.lhs)
             v = self.value(node.rhs)
             op = ASSIGN_OPS[node.op]
-            if dst != v: self.emit(f"    {op} {dst}, {v}")
+            if dst != v or node.op != "=": self.emit(f"    {op} {dst}, {v}")
 
         elif isinstance(node, While):
             L = self.label("while")
@@ -534,11 +620,10 @@ class Codegen:
             self.branch(node.cond, T, F)
             self.emit_label(T)
             for st in node.then: self.stmt(st)
-            if node.else_:
+            if node.els:
                 self.emit(f"    jmp {E}")
                 self.emit_label(F)
-                for st in node.else_:
-                    self.stmt(st)
+                for st in node.els: self.stmt(st)
                 self.emit_label(E)
             else:
                 self.emit_label(F)
@@ -558,17 +643,21 @@ class Codegen:
             sys.exit("unsupported statement")
 
 
-    def compile(self, decls, funcs):
+    def compile(self, ast):
+        self.expr_types = {}
         self.lines      = []
         self.tmp_i      = 0
         self.label_i    = 0
-        self.decls      = decls
-        self.funcs      = funcs
+        self.decls      = ast.decls
+        self.funcs      = ast.funcs
+        self.structs    = ast.structs
+
         self.tmp_vars   = {} # use dict to keep original order
         self.loop_stack = []
+        self.ast        = ast
 
         # functions
-        for f in funcs.values():
+        for f in self.funcs.values():
             self.emit("")
             self.emit(f"    ; function {f.name}")
             self.emit(f"{f.name}:")
@@ -588,17 +677,17 @@ class Codegen:
                 self.emit(f"{v.name} = {v.addr}")
             else:
                 self.emit(f"{v.name} = {addr}")
-                if isinstance(v, VarArr): addr += v.size
-                else: addr += 1
+                addr += ast.type_size(v.type)
 
         return "\n".join(self.lines + lines) + "\n"
 
 
 
 def main(args):
-    decls, funcs = Parser().program(Path(args.src))
+    parser = Parser()
+    parser.program(Path(args.src))
 
-    asm = Codegen().compile(decls, funcs)
+    asm = Codegen().compile(parser)
     out = args.out or Path(args.src).with_suffix(".asm")
     with open(out, "w") as f:
         f.write(asm)
